6d2e17d6bc74d76167e1f4ef0c474d587a9edae7
taek <yoda3708@gmail.com>
2021-11-13 20:19:57 +0900

    change : .prettierrc.js

--- App.tsx
+++ App.tsx
@@ -1,36 +1,39 @@
-import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NativeBaseProvider, extendTheme } from 'native-base';
-import { makeStore } from './redux';
-import { makeTheme } from './theme';
+import React from 'react'
+import {Provider as ReduxProvider} from 'react-redux'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {NativeBaseProvider, extendTheme} from 'native-base'
+import {makeStore} from './redux'
+import {makeTheme} from './theme'
 
-import Initial from './src/initial/initial';
-import LoginIndex from './src/login/loginIndex';
-import Category from './src/category/category';
-import MainIndex from './src/main/mainIndex';
+import Initial from './src/initial/initial'
+import LoginIndex from './src/login/loginIndex'
+import Category from './src/category/category'
+import MainIndex from './src/main/mainIndex'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
-const store = makeStore();
+const store = makeStore()
 
-const theme = makeTheme();
+const theme = makeTheme()
 
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <ReduxProvider store={store}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Initial"
-            screenOptions={{ headerShown: false }}>
+          <Stack.Navigator
+            initialRouteName="Initial"
+            screenOptions={{headerShown: false}}>
             <Stack.Screen name="Initial" component={Initial}></Stack.Screen>
-            <Stack.Screen name="LoginIndex" component={LoginIndex}></Stack.Screen>
+            <Stack.Screen
+              name="LoginIndex"
+              component={LoginIndex}></Stack.Screen>
             <Stack.Screen name="Category" component={Category}></Stack.Screen>
             <Stack.Screen name="MainIndex" component={MainIndex}></Stack.Screen>
           </Stack.Navigator>
         </NavigationContainer>
       </ReduxProvider>
     </NativeBaseProvider>
-  );
+  )
 }
--- theme.ts
+++ theme.ts
@@ -1,41 +1,41 @@
-import { extendTheme } from 'native-base';
+import {extendTheme} from 'native-base'
 
 export const makeTheme = () => {
-    const theme = extendTheme({
-        fontConfig: {
-            Roboto: {
-                100: {
-                    normal: 'Roboto-Light',
-                    italic: 'Roboto-LightItalic',
-                },
-                200: {
-                    normal: 'Roboto-Light',
-                    italic: 'Roboto-LightItalic',
-                },
-                300: {
-                    normal: 'Roboto-Light',
-                    italic: 'Roboto-LightItalic',
-                },
-                400: {
-                    normal: 'Roboto-Regular',
-                    italic: 'Roboto-Italic',
-                },
-                500: {
-                    normal: 'Roboto-Medium',
-                },
-                600: {
-                    normal: 'Roboto-Medium',
-                    italic: 'Roboto-MediumItalic',
-                },
-            },
+  const theme = extendTheme({
+    fontConfig: {
+      Roboto: {
+        100: {
+          normal: 'Roboto-Light',
+          italic: 'Roboto-LightItalic'
         },
-
-        font: {
-            heading: 'Roboto',
-            body: 'Roboto',
-            mono: 'Roboto',
+        200: {
+          normal: 'Roboto-Light',
+          italic: 'Roboto-LightItalic'
         },
-    });
+        300: {
+          normal: 'Roboto-Light',
+          italic: 'Roboto-LightItalic'
+        },
+        400: {
+          normal: 'Roboto-Regular',
+          italic: 'Roboto-Italic'
+        },
+        500: {
+          normal: 'Roboto-Medium'
+        },
+        600: {
+          normal: 'Roboto-Medium',
+          italic: 'Roboto-MediumItalic'
+        }
+      }
+    },
 
-    return theme;
+    font: {
+      heading: 'Roboto',
+      body: 'Roboto',
+      mono: 'Roboto'
+    }
+  })
+
+  return theme
 }
