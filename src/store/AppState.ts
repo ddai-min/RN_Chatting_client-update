bfc41e0d3e436c6c8bd522cfb02276892381f5da
taek <yoda3708@gmail.com>
2021-11-15 20:42:40 +0900

    change: rootReducer

--- src/store/AppState.ts
+++ src/store/AppState.ts
@@ -1,4 +1,8 @@
 import * as L from './login'
+import * as S from './signup'
+import * as CCR from './createChatRoom'
 export type AppState = {
   login: L.State
+  signup: S.State
+  createChatRoom: CCR.State
 }
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,10 @@
 import {combineReducers} from 'redux'
 import * as L from './login'
+import * as S from './signup'
+import * as CCR from './createChatRoom'
 
 export const rootReducer = combineReducers({
-  login: L.reducer
+  login: L.reducer,
+  signup: S.reducer,
+  createChatRoom: CCR.reducer
 })
