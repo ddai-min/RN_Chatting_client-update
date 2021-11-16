d787d0e421884357457499ebfca5c131e28f3667
taek <yoda3708@gmail.com>
2021-11-16 21:06:31 +0900

    add : interpolate

--- src/screens/login/initial.tsx
+++ src/screens/login/initial.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 import {Box, Text, Button, Center} from 'native-base'
 import {useNavigation} from '@react-navigation/native'
 
+import {AppState} from '../../store'
+import * as L from '../../store/login'
+
 export default function Initial() {
   const navigation = useNavigation()
+
+  const login = useSelector<AppState, L.State>((state) => state.login)
+  const {loggedIn, loggedUser} = login
+  const {email} = loggedUser
+
+  if (!loggedIn) {
+    return (
+      <Box flex={1} bg="white" safeAreaTop>
+        <Center flex={1}>
+          <Text fontSize="50">안녕하세요.</Text>
+        </Center>
+        <Button
+          h="8%"
+          rounded="none"
+          onPress={() => {
+            navigation.navigate('LoginIndex')
+          }}>
+          <Text color="white" fontSize="20">
+            다음
+          </Text>
+        </Button>
+      </Box>
+    )
+  }
 
   return (
     <Box flex={1} bg="white" safeAreaTop>
@@ -14,7 +42,7 @@ export default function Initial() {
         h="8%"
         rounded="none"
         onPress={() => {
-          navigation.navigate('LoginIndex')
+          navigation.navigate('MainIndex')
         }}>
         <Text color="white" fontSize="20">
           다음
