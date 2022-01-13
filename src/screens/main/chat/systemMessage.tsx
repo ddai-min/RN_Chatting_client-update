08673f00f81019f5c77c3e83eb817707893e5de9
taek <yoda3708@gmail.com>
2022-01-13 17:48:12 +0900

    add : chatIndexUI

---
+++ src/screens/main/chat/systemMessage.tsx
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class SystemMessage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      body: '',
+      time: ''
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.systemMessage}>
+        <Text style={styles.systemMessageText}>
+          {this.props.body.toUpperCase()}
+        </Text>
+
+        <View style={styles.timeContainer}>
+          <Text style={styles.systemMessageText}>{this.props.time}</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default SystemMessage
+
+const styles = StyleSheet.create({
+  systemMessage: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 32
+  },
+  systemMessageText: {
+    color: '#FFFFFF',
+    fontSize: 13,
+    fontWeight: '700'
+  },
+  timeContainer: {
+    flexDirection: 'row'
+  },
+  icon: {
+    marginRight: 4
+  }
+})
